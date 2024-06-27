41d889d657b3d3c543f8ab78179b16a7c4eded89
Jules Ivan Vital <jimvital42@gmail.com>
2024-06-27 14:18:53 +0800

    feat: update tech stack

--- portfolio/src/sections/About/TechStack.tsx
+++ portfolio/src/sections/About/TechStack.tsx
@@ -11,27 +11,35 @@ import {
 import { BsCircleFill } from "react-icons/bs";
 import { DiJava, DiJavascript, DiPython } from "react-icons/di";
 import {
+  FaAws,
   FaCss3Alt,
   FaGitAlt,
   FaHtml5,
   FaJira,
   FaNodeJs,
+  FaNpm,
   FaReact,
   FaSass,
+  FaYarn,
 } from "react-icons/fa";
 import {
+  SiBitbucket,
+  SiGraphql,
   SiJest,
   SiMongodb,
   SiNestjs,
   SiNextdotjs,
+  SiNotion,
   SiPerforce,
   SiRedux,
   SiRobotframework,
   SiTailwindcss,
   SiTwilio,
   SiTypescript,
+  SiWebpack,
 } from "react-icons/si";
 import { TbApi } from "react-icons/tb";
+import { BiLogoPostgresql } from "react-icons/bi";
 
 const FrontendTags = [
   { icon: FaReact, label: "ReactJS" },
@@ -43,12 +51,14 @@ const FrontendTags = [
   { icon: TbApi, label: "REST" },
   { icon: SiNextdotjs, label: "NextJS" },
   { icon: SiTailwindcss, label: "Tailwind CSS" },
+  { icon: SiGraphql, label: "GraphQL" },
 ];
 
 const BackendTags = [
   { icon: FaNodeJs, label: "NodeJS" },
   { icon: SiNestjs, label: "NestJS" },
   { icon: SiMongodb, label: "MongoDB" },
+  { icon: BiLogoPostgresql, label: "PostgreSQL" },
 ];
 
 const LanguageTags = [
@@ -64,16 +74,25 @@ const FrameworkTags = [
 
 const ToolTags = [
   { icon: FaGitAlt, label: "Git" },
+  { icon: SiBitbucket, label: "Bitbucket" },
   { icon: FaJira, label: "Jira" },
+  { icon: SiNotion, label: "Notion" },
   { icon: SiPerforce, label: "Perforce" },
-  { icon: SiTwilio, label: "SendGrid" },
+  { icon: SiTwilio, label: "Twilio SendGrid" },
+  { icon: FaNpm, label: "NPM" },
+  { icon: FaYarn, label: "Yarn" },
+  { icon: SiWebpack, label: "Webpack" },
+];
+
+const ServiceTags = [
+  { icon: FaAws, label: "AWS (Cognito, S3, Lambda, Amplify, AppSync)" },
 ];
 
 const TechStack: React.FC = () => {
   const renderTags = (data: { icon: As; label: string }[]) =>
     data.map(({ icon, label }) => (
-      <Tag key={label} variant="subtle" size="lg" width="max-content">
-        <TagLeftIcon boxSize={6} as={icon} />
+      <Tag key={label} variant="subtle" size="sm" width="max-content">
+        <TagLeftIcon boxSize={4} as={icon} />
         <TagLabel>{label}</TagLabel>
       </Tag>
     ));
@@ -89,14 +108,21 @@ const TechStack: React.FC = () => {
       </Flex>
       <Flex gap={4} wrap="wrap" alignItems="center">
         <Text fontWeight="bold" color="#34a0a4">
-          LANGUAGES
+          BACKEND
+        </Text>
+        <BsCircleFill size="0.75em" color="#4A5568" />
+        {renderTags(BackendTags)}
+      </Flex>
+      <Flex gap={4} wrap="wrap" alignItems="center">
+        <Text fontWeight="bold" color="#34a0a4">
+          PROGRAMMING LANGUAGES
         </Text>
         <BsCircleFill size="0.75em" color="#4A5568" />
         {renderTags(LanguageTags)}
       </Flex>
       <Flex gap={4} wrap="wrap" alignItems="center">
         <Text fontWeight="bold" color="#34a0a4">
-          FRAMEWORKS
+          TEST FRAMEWORKS
         </Text>
         <BsCircleFill size="0.75em" color="#4A5568" />
         {renderTags(FrameworkTags)}
@@ -110,10 +136,10 @@ const TechStack: React.FC = () => {
       </Flex>
       <Flex gap={4} wrap="wrap" alignItems="center">
         <Text fontWeight="bold" color="#34a0a4">
-          BACKEND
+          SERVICES
         </Text>
         <BsCircleFill size="0.75em" color="#4A5568" />
-        {renderTags(BackendTags)}
+        {renderTags(ServiceTags)}
       </Flex>
     </Stack>
   );
